ce86118488688a82e434243cd3a8d1a4bc1511a8
normcyr <norm@normandcyr.com>
2026-02-20 15:16:28 -0500

    chore(migrations): add initial alembic revision (autogenerated)

---
+++ alembic/versions/902caf0a8835_init_models.py
@@ -0,0 +1,20 @@
+"""init models
+
+Revision ID: 902caf0a8835
+Revises:
+Create Date: 2026-02-20 15:13:50.942027
+"""
+
+# revision identifiers, used by Alembic.
+revision = "902caf0a8835"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
